b525d06a238de60f8258bfeaddbbafc24aacadf4
priyankakanse123 <priyankakanse93@gmail.com>
2016-01-29 14:13:52 +0530

    first push

--- imagePicker/ViewController.swift
+++ imagePicker/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+        @IBOutlet var imageView: UIImageView!
+        
+        let imagePicker = UIImagePickerController()
+        
+        
+        @IBAction func loadImageButtonTapped(sender: UIButton) {
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .PhotoLibrary
+            
+            presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            imagePicker.delegate = self
+        }
+        
+        // MARK: - UIImagePickerControllerDelegate Methods
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+{
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .ScaleAspectFit
+            imageView.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+//    
+//        func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+//            if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+//                imageView.contentMode = .ScaleAspectFit
+//                imageView.image = pickedImage
+//            }
+//            
+//            dismissViewControllerAnimated(true, completion: nil)
+//        }
+    
+        func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+            dismissViewControllerAnimated(true, completion: nil)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
-}
+
 
